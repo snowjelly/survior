474840dc50881567440ed3badd8f829dacaf0a70
snowjelly <broadwellconnor@gmail.com>
2024-03-04 08:14:26 -0600

    initial commit (i forgot to track the work ive been doing)

---
+++ scenes/game_object/player/player.gd
@@ -0,0 +1,85 @@
+extends CharacterBody2D
+
+const MAX_SPEED = 125
+const ACCELERATION_SMOOTHING = 25
+
+@onready var damage_interval_timer = $DamageIntervalTimer
+@onready var health_component = $HealthComponent
+@onready var health_bar = $HealthBar
+@onready var abilities = $Abilities
+@onready var animation_player = $AnimationPlayer
+@onready var visuals = $Visuals
+
+var number_colliding_bodies = 0
+
+
+func _ready():
+	$CollisionArea2D.body_entered.connect(on_body_entered)
+	$CollisionArea2D.body_exited.connect(on_body_exited)
+	damage_interval_timer.timeout.connect(on_damage_interval_timer_timeout)
+	health_component.health_changed.connect(on_health_changed)
+	GameEvents.ability_upgrade_added.connect(on_ability_upgrade_added)
+	update_health_display()
+
+
+func _process(delta):
+	var movement_vector = get_movement_vector()
+	var direction = movement_vector.normalized()
+	var target_velocity = direction * MAX_SPEED
+	
+	velocity = velocity.lerp(target_velocity, 1 - exp(-delta * ACCELERATION_SMOOTHING))
+	
+	move_and_slide()
+	
+	if movement_vector.x != 0 || movement_vector.y != 0:
+		var move_sign_x = sign(movement_vector.x)
+		var move_sign_y = sign(movement_vector.y)
+		if move_sign_x > 0:
+			animation_player.play("walk_right")
+		if move_sign_x < 0:
+			animation_player.play('walk_left')
+		else:
+			animation_player.play('walk_right')
+	else:
+		animation_player.play("RESET")
+
+
+func get_movement_vector():
+	var x_movement = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	var y_movement = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+	return Vector2(x_movement, y_movement)
+
+
+func check_deal_damage():
+	if number_colliding_bodies == 0 || !damage_interval_timer.is_stopped():
+		return
+	health_component.damage(1)
+	damage_interval_timer.start()
+
+
+func update_health_display():
+	health_bar.value = health_component.get_health_percent()
+
+
+func on_body_entered(other_body: Node2D):
+	number_colliding_bodies += 1
+	check_deal_damage()
+
+
+func on_body_exited(other_body: Node2D):
+	number_colliding_bodies -= 1
+
+
+func on_damage_interval_timer_timeout():
+	check_deal_damage()
+
+func on_health_changed():
+	update_health_display()
+
+
+func on_ability_upgrade_added(ability_upgrade: AbilityUpgrade, current_upgrades: Dictionary):
+	if not ability_upgrade is Ability:
+		return
+	
+	var ability = ability_upgrade as Ability
+	abilities.add_child(ability_upgrade.ability_controller_scene.instantiate())
